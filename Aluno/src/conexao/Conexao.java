75191865f2c0765c747be05a7e82942388531644
Administrador <Administrador@10.10.4.16>
2019-06-27 15:30:33 -0300

    lalalalla

--- Aluno/src/conexao/Conexao.java
+++ Aluno/src/conexao/Conexao.java
@@ -1,3 +1,4 @@
+package conexao;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -14,6 +15,8 @@ import java.util.logging.Logger;
  * @author edimar
  */
 public class Conexao {
+    
+   
 
     private static Connection conn;
     private final static String driver = "org.postgresql.Driver";
---
+++ Aluno/src/dao/AlunoDao.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import modelo.Aluno;
+
+/**
+ *
+ * @author Administrador
+ */
+public class AlunoDao {
+
+    public static boolean inserir(Aluno objeto) {
+        String sql = "INSERT INTO aluno (emdereco, nome) VALUES (?, ?)";
+        try {
+            PreparedStatement ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ps.setString(1, objeto.getEndereco());
+            ps.setString(2, objeto.getNome());
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Aluno objeto = new Aluno();
+        objeto.setEndereco("Brasil");
+        objeto.setNome("Maria");
+
+        boolean resultado = inserir(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Inserido com sucesso!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+    }
+
+}
